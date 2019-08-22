c11a57afadb2001c757b4298f0d21e5885f99229
Andrew Smith <codingmba@gmail.com>
2019-08-22 07:59:09 -0400

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding>
+    <input type="text" v-model="message" @keyup.esc="clearMessage" @keyup.enter="alertMessage" />
+    <button @click="clearMessage">Clear</button>
+    <h5 class="border-gray" v-show="message.length">{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'I love Vue.js!',
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage() {
+      alert(this.message)
+    },
+  },
+}
+</script>
+
+<style>
+.border-gray {
+  border: 1px solid gray;
+}
+</style>
